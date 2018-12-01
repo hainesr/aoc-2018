dbf300bb42ff750fe9e5cffc58795e87240f0ad0
Robert Haines <rhaines@manchester.ac.uk>
2018-12-01 18:02:03 +0000

    Solution for day 1, part 2.

--- lib/aoc2018/chronal_calibration.rb
+++ lib/aoc2018/chronal_calibration.rb
@@ -17,10 +17,28 @@ module AOC2018
       end
 
       puts "Part 1: #{add_freqs(input)}"
+      puts "Part 2: #{find_repeated_freq(input)}"
     end
 
     def add_freqs(input)
       input.reduce(:+)
+    end
+
+    def find_repeated_freq(input)
+      acc = 0
+      seen = { 0 => true }
+
+      loop do
+        input.each do |freq|
+          acc += freq
+
+          if seen[acc]
+            return acc
+          else
+            seen[acc] = true
+          end
+        end
+      end
     end
 
   end
--- test/chronal_calibration_test.rb
+++ test/chronal_calibration_test.rb
@@ -20,7 +20,14 @@ class AOC2018::ChronalCalibrationTest < Minitest::Test
     assert_equal 3, @cc.add_freqs([1, 1, 1])
     assert_equal 0, @cc.add_freqs([1, 1, -2])
     assert_equal -6, @cc.add_freqs([-1, -2, -3])
+  end
 
+  def test_find_repeated_freq
+    assert_equal 2, @cc.find_repeated_freq([1, -2, 3, 1])
+    assert_equal 0, @cc.find_repeated_freq([1, -1])
+    assert_equal 10, @cc.find_repeated_freq([3, 3, 4, -2, -4])
+    assert_equal 5, @cc.find_repeated_freq([-6, 3, 8, 5, -6])
+    assert_equal 14, @cc.find_repeated_freq([7, 7, -2, -7, -4])
   end
 
 end
