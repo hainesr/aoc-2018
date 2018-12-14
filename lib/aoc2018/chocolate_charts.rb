8345e49b55cf9dcad65597f4e1c85c8607092bdb
Robert Haines <rhaines@manchester.ac.uk>
2018-12-14 23:35:27 +0000

    Solution for day 14, part 2.

--- lib/aoc2018/chocolate_charts.rb
+++ lib/aoc2018/chocolate_charts.rb
@@ -11,15 +11,18 @@ require 'aoc2018'
 module AOC2018
   class ChocolateCharts < Day
     def run
-      puts "Part 1: #{practice_scores(635041)}"
+      scores = practice_scores(16_000_000)
+
+      puts "Part 1: #{next_ten(635041, scores)}"
+      puts "Part 2: #{find_sequence_index('635041', scores)}"
     end
 
-    def practice_scores(num, initial = [3, 7])
-      scores = initial.dup
+    def practice_scores(num)
+      scores = [3, 7]
       elf1 = 0
       elf2 = 1
 
-      loop do
+      num.times do
         elf1_s = scores[elf1]
         elf2_s = scores[elf2]
         total_s = elf1_s + elf2_s
@@ -31,13 +34,19 @@ module AOC2018
         end
 
         len_s = scores.length
-        break if len_s >= num + 10
-
         elf1 = (elf1 + elf1_s + 1) % len_s
         elf2 = (elf2 + elf2_s + 1) % len_s
       end
 
+      scores
+    end
+
+    def next_ten(num, scores)
       scores[num, 10].map(&:to_s).join
+    end
+
+    def find_sequence_index(seq, scores)
+      scores.map(&:to_s).join.index(seq)
     end
   end
 end
--- test/chocolate_charts_test.rb
+++ test/chocolate_charts_test.rb
@@ -14,10 +14,21 @@ class AOC2018::ChocolateChartsTest < Minitest::Test
     @cc = AOC2018::ChocolateCharts.new
   end
 
-  def test_practice_scores
-    assert_equal '0124515891', @cc.practice_scores(5)
-    assert_equal '5158916779', @cc.practice_scores(9)
-    assert_equal '9251071085', @cc.practice_scores(18)
-    assert_equal '5941429882', @cc.practice_scores(2018)
+  def test_next_ten
+    scores = @cc.practice_scores(2_500)
+
+    assert_equal '0124515891', @cc.next_ten(5, scores)
+    assert_equal '5158916779', @cc.next_ten(9, scores)
+    assert_equal '9251071085', @cc.next_ten(18, scores)
+    assert_equal '5941429882', @cc.next_ten(2018, scores)
+  end
+
+  def test_find_sequence_index
+    scores = @cc.practice_scores(2_500)
+
+    assert_equal 9, @cc.find_sequence_index('51589', scores)
+    assert_equal 5, @cc.find_sequence_index('01245', scores)
+    assert_equal 18, @cc.find_sequence_index('92510', scores)
+    assert_equal 2018, @cc.find_sequence_index('59414', scores)
   end
 end
