7636e91c2a971977e9f1d6740e04f8bbee3da703
Robert Haines <rhaines@manchester.ac.uk>
2018-12-29 18:36:19 +0000

    Solution for day 23, part2.
    Uses the Z3 solver from MSR.
    
    But this is going to break the CI build, isn't it?..

--- lib/aoc2018/experimental_emergency_teleportation.rb
+++ lib/aoc2018/experimental_emergency_teleportation.rb
@@ -7,6 +7,7 @@
 # Public Domain
 
 require 'aoc2018'
+require 'z3'
 
 module AOC2018
   class ExperimentalEmergencyTeleportation < Day
@@ -15,6 +16,7 @@ module AOC2018
       bots = parse_input(input)
 
       puts "Part 1: #{in_range(bots)}"
+      puts "Part 2: #{manhattan([0, 0, 0], most_in_range(bots))}"
     end
 
     def in_range(bots)
@@ -22,6 +24,30 @@ module AOC2018
       bots.reduce(0) do |num, (pos, _)|
         manhattan(str_pos, pos) <= str ? num + 1 : num
       end
+    end
+
+    def most_in_range(bots)
+      x = Z3.Int('x')
+      y = Z3.Int('y')
+      z = Z3.Int('z')
+      point = [x, y, z]
+
+      cost = x * 0
+      bots.each do |pos, r|
+        cost += Z3.IfThenElse(z3_manhattan(point, pos) <= r, 1, 0)
+      end
+
+      optimizer = Z3::Optimize.new
+
+      # Maximise for the number of bots in range...
+      optimizer.maximize(cost)
+
+      # Then minimize the distance from the origin for the tie-breaker.
+      optimizer.minimize(z3_manhattan([0, 0, 0], [x, y, z]))
+      optimizer.check
+      model = optimizer.model
+
+      [model[x].to_i, model[y].to_i, model[z].to_i]
     end
 
     def parse_input(input)
@@ -37,6 +63,16 @@ module AOC2018
 
     def manhattan(p1, p2)
       (p1[0] - p2[0]).abs + (p1[1] - p2[1]).abs + (p1[2] - p2[2]).abs
+    end
+
+    private
+
+    def z3_abs(x)
+      Z3.IfThenElse(x >= 0, x, -x)
+    end
+
+    def z3_manhattan(p1, p2)
+      z3_abs(p1[0] - p2[0]) + z3_abs(p1[1] - p2[1]) + z3_abs(p1[2] - p2[2])
     end
   end
 end
--- test/experimental_emergency_teleportation_test.rb
+++ test/experimental_emergency_teleportation_test.rb
@@ -10,7 +10,7 @@ require 'test_helper'
 require 'aoc2018/experimental_emergency_teleportation'
 
 class AOC2018::ExperimentalEmergencyTeleportationTest < Minitest::Test
-  INPUT = [
+  INPUT1 = [
     'pos=<0,0,0>, r=4',
     'pos=<1,0,0>, r=1',
     'pos=<4,0,0>, r=3',
@@ -22,7 +22,7 @@ class AOC2018::ExperimentalEmergencyTeleportationTest < Minitest::Test
     'pos=<1,3,1>, r=1'
   ].freeze
 
-  PARSED = {
+  PARSED1 = {
     [0, 0, 0] => 4,
     [1, 0, 0] => 1,
     [4, 0, 0] => 3,
@@ -34,19 +34,33 @@ class AOC2018::ExperimentalEmergencyTeleportationTest < Minitest::Test
     [1, 3, 1] => 1
   }.freeze
 
+  INPUT2 = [
+    'pos=<10,12,12>, r=2',
+    'pos=<12,14,12>, r=2',
+    'pos=<16,12,12>, r=4',
+    'pos=<14,14,14>, r=6',
+    'pos=<50,50,50>, r=200',
+    'pos=<10,10,10>, r=5'
+  ].freeze
+
   def setup
     @eet = AOC2018::ExperimentalEmergencyTeleportation.new
   end
 
   def test_parse_input
-    assert_equal PARSED, @eet.parse_input(INPUT)
+    assert_equal PARSED1, @eet.parse_input(INPUT1)
   end
 
   def test_find_strongest
-    assert_equal [[0, 0, 0], 4], @eet.find_strongest(PARSED)
+    assert_equal [[0, 0, 0], 4], @eet.find_strongest(PARSED1)
   end
 
   def test_in_range
-    assert_equal 7, @eet.in_range(PARSED)
+    assert_equal 7, @eet.in_range(PARSED1)
+  end
+
+  def test_most_in_range
+    bots = @eet.parse_input(INPUT2)
+    assert_equal [12, 12, 12], @eet.most_in_range(bots)
   end
 end
